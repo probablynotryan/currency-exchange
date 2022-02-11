17492d9b263ae6c1ee8413df8e6151d4e9fd9917
Ryan Bass <theryanbass@gmail.com>
2022-02-11 13:34:02 -0800

    compartmentalize validation functions from UI

--- src/index.js
+++ src/index.js
@@ -3,41 +3,29 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ConvertThat from './convertmoney.js';
-
-function checkResponse(res){
-  if (res.result != 'success') {
-    $('#response-text').text('An error occured attempting to fetch from API.');
-  } else if (res.result === 'success'){
-    $('p').show();
-    $('#go').show();
-    $('#user-input').show();
-    $('.currency-select').show();
-    return res.conversion_rates;
-  } else {
-    $('#response-text').text('An unknown error occurred.');
-  }
-}
-
-function checkInput(input){
-  if (input === '' || isNaN(input)) {
-    $('#response-text').text('Come on, silly. Enter a number, please.');
-  } else if (!isNaN(input)){
-    let conversionOutput = input / $('#currency-in').val() * $('#currency-out').val();
-    $('#response-text').text(conversionOutput);
-  } else {
-    $('#response-text').text("What have you done now? We don't know what went wrong.");
-  }
-}
+import {sanitizeInputs, checkResponse} from './validate.js';
 
 $(document).ready(function() {
   ConvertThat.moneyMoney()
     .then(function(res){
-      Object.entries(checkResponse(res)).forEach(element => {
+      if (checkResponse(res)) {
+        $('p').show();
+        $('#go').show();
+        $('#user-input').show();
+        $('.currency-select').show();
+      } else {
+        $('#response-text').text('An error occured attempting to fetch from API.');
+        return;
+      }
+      Object.entries(res.conversion_rates).forEach(element => {
         $('.currency-select').append(`<option value = "${element[1]}"> ${element[0]} </option>`);
       });
     });
   $("#go").click(function() {
     let userInput = parseInt($('#user-input').val());
-    checkInput(userInput);   
+    let inCurrencyRate = $('#currency-in').val();
+    let outCurrencyRate = $('#currency-out').val();
+    let outCurrencyType = $("#currency-out option:selected" ).text();
+    $('#response-text').text(sanitizeInputs(userInput, inCurrencyRate, outCurrencyRate, outCurrencyType));
   });
 });
---
+++ src/validate.js
@@ -0,0 +1,19 @@
+export function checkResponse(res){
+  if (res.result != 'success') {
+    return -1;
+  } else if (res.result === 'success'){
+    return 1;
+  } else {
+    return -1;
+  }
+}
+
+export function sanitizeInputs(input, inCurrency, outCurrency, outCurrencyType){
+  if (input === '' || isNaN(input)) {
+    return ('Come on, silly. Enter a number, please.');
+  } else if (!isNaN(input)){
+    return ((input / inCurrency * outCurrency).toFixed(2) + " " + outCurrencyType);
+  } else {
+    return ('An error occured');
+  }
+}
