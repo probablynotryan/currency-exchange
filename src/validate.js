a8a79e57e527fe91e8118f21fca558bd12ee18b0
Ryan Bass <theryanbass@gmail.com>
2022-02-11 14:08:59 -0800

    add ka-ching sound when conversion check returns a value

--- src/validate.js
+++ src/validate.js
@@ -8,10 +8,11 @@ export function checkResponse(res){
   }
 }
 
-export function sanitizeInputs(input, inCurrency, outCurrency, outCurrencyType){
+export function sanitizeInputs(input, inCurrency, outCurrency, outCurrencyType, kaChing){
   if (input === '' || isNaN(input)) {
     return ('Come on, silly. Enter a number, please.');
   } else if (!isNaN(input)){
+    kaChing.play();
     return ((input / inCurrency * outCurrency).toFixed(2) + " " + outCurrencyType);
   } else {
     return ('An error occured');
