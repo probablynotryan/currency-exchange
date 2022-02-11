e37067e533198aeddde4a95cd50e952b9ff19bcd
Ryan Bass <theryanbass@gmail.com>
2022-02-11 09:19:58 -0800

    test fetch response at docready

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import ConvertThat from './convertmoney.js';
+
+$(document).ready(function() {
+
+  ConvertThat.moneyMoney()
+    .then(function(res) {
+      console.log(res);
+    });
+});
